0ae8313962233fd0bdc49cce34b7e4505469d4cb
Tom Marien <tommarien@gmail.com>
2019-10-15 11:09:03 +0200

    feat(alert): Added AlertHeader

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import Alert from './components/Alert/Alert';
+import AlertHeader from './components/Alert/AlertHeader';
 
 export function App() {
   return (
     <>
       <h1>Hello from ES2015+</h1>
-      <div className="main container-fluid" />
+      <div className="main container-fluid">
+        <Alert dismissible>
+          <AlertHeader>Title</AlertHeader>
+          This is a description
+        </Alert>
+      </div>
     </>
   );
 }
